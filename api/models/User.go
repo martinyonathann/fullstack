99f4dc8133ee3bf7d1bd8bc4f216b346911466e3
NB_Martin <martin.y@inception.asia>
2020-08-18 11:53:12 +0700

    add post and fixing user

--- api/models/User.go
+++ api/models/User.go
@@ -3,6 +3,7 @@ package models
 import (
 	"errors"
 	"html"
+	"log"
 	"strings"
 	"time"
 
@@ -89,20 +90,66 @@ func (u *User) Validatte(action string) error {
 		return nil
 	}
 }
-func (u *User) saveUser(db *gorm.DB)(*User, error){
+func (u *User) saveUser(db *gorm.DB) (*User, error) {
 	var err error
 	err = db.Debug().Create(&u).Error
-	if err != nil{
+	if err != nil {
 		return &User{}, err
 	}
 	return u, nil
 }
-func (u *User) findAllUsers (db *gorm.DB)(*[]User, error){
+func (u *User) findAllUsers(db *gorm.DB) (*[]User, error) {
 	var err error
 	users := []User{}
 	err = db.Debug().Model(&User{}).Limit(100).Find(&users).Error
-	if err != nil{
+	if err != nil {
 		return &[]User{}, err
 	}
 	return &users, err
+}
+
+func (u *User) findUserByID(db *gorm.DB, uid uint32) (*User, error) {
+	var err error
+	err = db.Debug().Model(User{}).Where("id = ?", uid).Take(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &User{}, err
+	}
+	return u, err
+}
+
+func (u *User) UpdateAUser(db *gorm.DB, uid uint32) (*User, error) {
+
+	//To hash the password
+	err := u.BeforeSave()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = db.Debug().Model(&User{}).Where("id = ?", uid).Take(&User{}).UpdateColumns(
+		map[string]interface{}{
+			"password":  u.Password,
+			"nickname":  u.Nickname,
+			"email":     u.Email,
+			"update_at": time.Now,
+		},
+	)
+	if db.Error != nil {
+		return &User{}, db.Error
+	}
+	//This is the display the updated user
+	err = db.Debug().Model(&User{}).Where("id = ?", uid).Take(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+func (u *User) DeleteAUser(db *gorm.DB, uid uint32) (int64, error) {
+	db = db.Debug().Model(&User{}).Where("id = ?", uid).Take(&User{}).Delete(&User{})
+	if db.Error != nil {
+		return 0, db.Error
+	}
+	return db.RowsAffected, nil
 }
