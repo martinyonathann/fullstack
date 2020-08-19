e8387164d4e94c5feba68dbbc1fde0be05133993
NB_Martin <martin.y@inception.asia>
2020-08-19 10:53:42 +0700

    fixing for middlewares

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/victorsteven/fullstack/api/auth"
-	"github.com/victorsteven/fullstack/api/responses"
+	"github.com/martinyonathann/fullstack/api/auth"
+	"github.com/martinyonathann/fullstack/api/responses"
 )
 
 func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
