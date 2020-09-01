2e62e15a0c5f3537bf269739aaeeeebc9348192c
NB_Martin <martin.y@inception.asia>
2020-09-01 13:52:46 +0700

    fixing and configure database

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -26,7 +26,7 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
 		server.DB, err = gorm.Open(Dbdriver, DBURL)
 		if err != nil {
-			fmt.Printf("Cannot connect to %s database", Dbdriver)
+			fmt.Printf("Cannot connect to %s database ", Dbdriver)
 			log.Fatal("This is the error:", err)
 		} else {
 			fmt.Printf("We are connected to the %s database", Dbdriver)
@@ -34,9 +34,10 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 	}
 	if Dbdriver == "postgres" {
 		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+		fmt.Printf("isi dari DBurl = " + DBURL)
 		server.DB, err = gorm.Open(Dbdriver, DBURL)
 		if err != nil {
-			fmt.Printf("Cannot connect to %s database", Dbdriver)
+			fmt.Printf("Cannot connect to %s database ", Dbdriver)
 			log.Fatal("This is the error :", err)
 		} else {
 			fmt.Printf("We are connected to the %s database", Dbdriver)
